aeec68beb82e93cb69df8d376f55c407179c411c
✓xAlbertx✓ <albertozaaz760@mail.paradoxmedia.space>
2025-05-18 18:08:45 +0300

    Update community.py

--- app/routes/community.py
+++ app/routes/community.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
 from app.models import User, factory, Community
+import random
+
 
 community_bp = Blueprint('community', __name__, template_folder='templates')
 
@@ -11,13 +13,18 @@ def community():
     user = db_session.get(User, user_id)
     if request.method == "GET":
         return render_template("community.html", user=user)
-    rname = request.form["rname"]
-    name = request.form["name"]
+    rname = str(request.form["rname"])
+    name = str(request.form["name"])
     email = str(request.form["email"])
     cost = int(request.form["cost"])
-    rarity = request.form["rarity"]
-    history = request.form["history"]
-    user_history = request.form["history_user"]
+    rarity = str(request.form["rarity"])
+    history = str(request.form["history"])
+    user_history = str(request.form["history_user"])
+    img = request.files["file"]
+    content = img.read()
+    out = open(f"static/img/img{random.randint(0, 999999)}.png", "wb")
+    out.write(content)
+    out.close()
     newitem = Community(rname=rname, user_id=user_id, name=name, email=email, cost=cost, rarity=rarity, history=history, history_user=user_history)
     db_session.add(newitem)
     db_session.commit()
